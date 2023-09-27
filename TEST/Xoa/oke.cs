d0374c7d4385c2c3a0681faff76b2d3da9c0fabb
Nguyễn Thanh Sơn <thanhsonms10@gmail.com>
2023-09-27 15:37:13 +0700

    update 17/09/2023 337

---
+++ TEST/Xoa/oke.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TEST.Xoa
+{
+    public class oke
+    {
+        public int ok { get; set; }
+    }
+}
---
+++ TEST/Xoa1/oke1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TEST.Xoa1
+{
+    public class oke1
+    {
+        public string output {get; set;}
+    }
+}
